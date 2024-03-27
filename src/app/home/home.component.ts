3b4735b0f4db6e328bbeaf8534056d10ee4c2eb2
Rona Mae Abesamis <ronamae.abesamis@gmail.com>
2024-03-27 20:10:03 +0800

    Add product component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,24 +1,30 @@
 import { Component } from '@angular/core';
-import { Products } from '../../types';
+import { Product, Products } from '../../types';
 import { ProductsService } from '../services/products.service';
+import { ProductComponent } from '../components/product/product.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ProductComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  constructor(
-    private productsService: ProductsService
-  ){}
+  constructor(private productsService: ProductsService) {}
+
+  products: Product[] = [];
+
+  onProductOutput(product: Product) {
+    console.log(product, 'Output');
+  }
 
   ngOnInit() {
     this.productsService
     .getProducts('http://localhost:3000/clothes', {page: 0, perPage: 5})
     .subscribe((products: Products) => {
-      console.log(products.items);
+      this.products = products.items;
     })
   }
 
