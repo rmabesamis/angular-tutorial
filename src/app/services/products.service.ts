dcdc1c71bce158f8ce8d12b1533661f01091ddb6
Rona Mae Abesamis <ronamae.abesamis@gmail.com>
2024-03-29 02:41:51 +0800

    add edit, create and delete buttons to UI

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -4,19 +4,30 @@ import { Observable } from 'rxjs';
 import { PaginationParams, Products } from '../../types';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductsService {
-
-  constructor(private apiService: ApiService) { }
+  constructor(private apiService: ApiService) {}
 
   getProducts = (
-    url: string, 
-    params: PaginationParams
+    url: string,
+    params: PaginationParams,
   ): Observable<Products> => {
     return this.apiService.get(url, {
       params,
       responseType: 'json',
     });
-  }
+  };
+
+  addProduct = (url: string, body: any): Observable<any> => {
+    return this.apiService.post(url, body, {});
+  };
+
+  editProduct = (url: string, body: any): Observable<any> => {
+    return this.apiService.put(url, body, {});
+  };
+
+  deleteProduct = (url: string): Observable<any> => {
+    return this.apiService.delete(url, {});
+  };
 }
